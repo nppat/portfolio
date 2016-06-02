3114d72952767469ab2525937f978e2fac92eeed
Patrick Adamson <nppat@hotmail.com>
2016-06-02 13:11:30 -0500

    added mt2.jpg to img file, updated index, jqeury, and style

--- jquery_functions.js
+++ jquery_functions.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
 	$("#tabs").tabs({
 		event: "mouseover"
 	});
+	//Changes image whenever the document is clicked
+	$(document).click(function(){
+		var src = $("img").attr('src');
+		$("img").attr('src', $("img").attr('data-alt-src'));
+		$("img").attr('data-alt-src', src);
+	});
 });
+
+
